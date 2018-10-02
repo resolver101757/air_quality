8e09e946d90ca8144140e2a991a854607ee4cf55
Alex Kelly <Alex.kelly@tinsleybridge.co.uk>
2018-10-02 20:34:04 +0100

    sintax error on else statement

--- air_quality.py
+++ air_quality.py
@@ -73,7 +73,7 @@ try:
                     output,
                     sensor.data.gas_resistance))
                 gas = sensor.data.gas_resistance
-            else
+            else:
                 gas = "no data"
                 
             data = [{"measurement": "bme680_sensor",
