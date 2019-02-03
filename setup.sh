3226dfdbe288f8f3299bfb9aadf1581c784d10fd
alex kelly <kelster101757@hotmail.com>
2019-02-03 19:17:17 +0000

    updated setup to setup services

--- setup.sh
+++ setup.sh
@@ -2,8 +2,14 @@
 # copy service file to service location 
 # Found this instruction to setup systemd script and permisions here : https://www.raspberrypi-spy.co.uk/2015/10/how-to-autorun-a-python-script-on-boot-using-systemd/ 
 
+#Air particles  
 cp -v -f air_particles.service /lib/systemd/system/
-chmod +x  /home/pi/ak_air_quality_sensor/read_pressure.py
+chmod +x  /home/pi/air_quality/air_particles.py
 sudo chmod 644 /lib/systemd/system/air_particles.service
 systemctl enable air_particles.service
 
+# Air quality (GAS)
+cp -v -f air_quality.service /lib/systemd/system/
+chmod +x  /home/pi/air_quality/air_quality.py
+sudo chmod 644 /lib/systemd/system/air_quality.service
+systemctl enable air_quality.service
